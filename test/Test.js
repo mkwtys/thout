b68f4dbdf4cc1911157ab6a5ccb8279988f1a79d
mkwtys <mkwtys@gmail.com>
2016-08-24 02:20:21 +0900

    test: add some test

--- test/Test.js
+++ test/Test.js
@@ -5,3 +5,23 @@ test('Test', (t) => {
   t.ok(Test);
   t.end();
 });
+
+test('Test#getTitle', (t) => {
+  let test = new Test();
+  t.ok(test.getTitle() === '');
+
+  test = new Test('test');
+  t.ok(test.getTitle() === 'test');
+
+  t.end();
+});
+
+test('Test#getFilename', (t) => {
+  let test = new Test();
+  t.ok(test.getFilename() === '');
+
+  test = new Test('test', undefined, 'test.js');
+  t.ok(test.getFilename() === 'test.js');
+
+  t.end();
+});
