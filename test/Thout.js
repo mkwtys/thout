66ff31e2de34cadeafb0f0bb1bd7ad7126c8e448
mkwtys <mkwtys@gmail.com>
2016-08-30 21:24:04 +0900

    fix: create Thout.js

---
+++ test/Thout.js
@@ -0,0 +1,47 @@
+'use strict';
+const sinon = require('sinon');
+const test = require('tape');
+const Thout = require('../lib/Thout');
+
+test('test function', (t) => {
+  const runner = {
+    addTest: sinon.spy(),
+    run: () => {},
+    on: () => {}
+  };
+  const thout = new Thout({ runner: runner });
+  t.ok(typeof thout === 'function');
+
+  thout();
+  t.ok(runner.addTest.called);
+
+  t.end();
+});
+
+test('skip', (t) => {
+  const runner = {
+    addTest: sinon.spy(),
+    run: () => {},
+    on: () => {}
+  };
+  const thout = new Thout({ runner: runner });
+  t.ok(typeof thout === 'function');
+
+  thout.skip('skip');
+  t.ok(runner.addTest.called);
+
+  t.end();
+});
+
+test('setup', (t) => {
+  const runner = {
+    setup: sinon.spy(),
+    run: () => {},
+    on: () => {}
+  };
+  const thout = new Thout({ runner: runner });
+  thout.setup({ test: 'test' });
+  t.ok(runner.setup.args);
+  t.ok(runner.setup.called);
+  t.end();
+});
